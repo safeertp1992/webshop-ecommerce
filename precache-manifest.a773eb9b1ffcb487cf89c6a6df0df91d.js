c1cc80fbb74840676466066e92be9980fe5246e8
Safeer Tazaha Padannayil <SafeerNajeeb@innocube.local>
2020-05-09 19:59:48 +0200

    Updates

---
+++ precache-manifest.a773eb9b1ffcb487cf89c6a6df0df91d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84775896bd2d75c11fd06bb205c8e57a",
+    "url": "/webshop-ecommerce/index.html"
+  },
+  {
+    "revision": "564bbc15041d6af0039b",
+    "url": "/webshop-ecommerce/static/css/main.fbe79bd4.chunk.css"
+  },
+  {
+    "revision": "a0807612eceab01ab3c1",
+    "url": "/webshop-ecommerce/static/js/2.b12c7bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webshop-ecommerce/static/js/2.b12c7bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564bbc15041d6af0039b",
+    "url": "/webshop-ecommerce/static/js/main.a9893657.chunk.js"
+  },
+  {
+    "revision": "9952c0e8900def3fcf6b",
+    "url": "/webshop-ecommerce/static/js/runtime-main.c21c33ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webshop-ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
